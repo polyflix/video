7ff9ab0a4eb0af5debc4b81e73f8bfcba8346f39
Malo Polese <malo.polese@etu.umontpellier.fr>
2022-06-06 14:57:19 +0200

    fix(minio): add logs and fix external psu

--- src/main/modules/infrastructure/services/token.service.ts
+++ src/main/modules/infrastructure/services/token.service.ts
@@ -62,17 +62,25 @@ export class TokenService {
         bucketName: MINIO_BUCKETS,
         path: string
     ): Promise<PresignedUrl> {
-        let psu: string = await this.minioClient
+        this.logger.log(
+            `Create PUT PSU for bucket ${bucketName} at path ${path}`
+        );
+        const psu: string = await this.minioClient
             .presignedPutObject(bucketName, path, PUT_PRESIGNED_URL_EXPIRY)
             .catch((err) => {
                 return this.minioErrorHandler(err);
             });
         const externalUri = this.getMinioBaseUri("external");
         const internalUri = this.getMinioBaseUri("internal");
+        const externalPsu = psu.replace(internalUri, externalUri);
 
-        psu = psu.replace(internalUri, externalUri);
+        this.logger.log(
+            `Internal uri ${internalUri} - External uri ${externalUri}`
+        );
+        this.logger.log(`internal psu ${psu}`);
+        this.logger.log(`external psu ${externalPsu}`);
 
-        return PresignedUrl.create({ tokenAccess: psu });
+        return PresignedUrl.create({ tokenAccess: externalPsu });
     }
 
     public getMinioBaseUri(type: "internal" | "external") {
@@ -80,6 +88,10 @@ export class TokenService {
             `minio.environment.${type}`
         );
         const protocol = ssl ? "https" : "http";
-        return `${protocol}://${host}:${port}`;
+        const baseUri = `${protocol}://${host}`;
+        if (port) {
+            return `${baseUri}:${port}`;
+        }
+        return baseUri;
     }
 }
